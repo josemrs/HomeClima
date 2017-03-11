e81a59b9dddd0e7c6d5dd8a1b9876881daae0048
Jose M Rodriguez <jose@jmrs.es>
2017-03-11 11:03:06 +0000

    Multiple minor changes
    Removed sensor test at 0x77 address
    Added funtion for MQTT publish
    Serial output reformat
    Added define NO_SERIAL_OUTPUT to disable in main loop (Default disabled)
    No reset if MQTT fails to connect

--- RemoteUnit.c
+++ RemoteUnit.c
@@ -14,6 +14,10 @@
 #include <Adafruit_BME280.h>  // SDA pin: ESP8266 GPIO4 -> NodeMcu D2.
                               // SCL pin: ESP8266 GPIO5 -> NodeMcu D3. 
 
+#define NO_SERIAL_OUTPUT
+
+const char OK_STR[] {"Ok"};
+const char FAIL_STR[] {"Failed"};
 const uint16_t MAIN_LOOP_DELAY = 10000;
 
 /* PIN definitions */
@@ -25,8 +29,9 @@ const int8_t OUTPUT_PIN = 10;   // ESP8266 GPIO10 -> NodeMcu SD3
 WiFiClient client;
 
 /* MQTT */
-#define MQTT_SERVER  "192.168.1.15"
+#define MQTT_SERVER "192.168.1.15"
 #define MQTT_PORT    1883
+#define MQTT_RETRIES 3
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT);
 
 /* MQTT Feeds */
@@ -40,7 +45,28 @@ Adafruit_BME280 bme;
 
 // Bug workaround for Arduino 1.6.6, it seems to need a function declaration
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
-void MQTT_connect();
+bool MQTT_connect();
+
+void restart()
+{
+  Serial.println("ESP restarting...");
+  delay(60000);
+  ESP.restart();
+}
+
+void publishTopic(Adafruit_MQTT_Publish* topic, float value)
+{
+  Serial.print("Publish: ");
+
+  if (!topic->publish(value))
+  {
+    Serial.println(FAIL_STR);
+  }
+  else
+  {
+    Serial.println(OK_STR);
+  }
+}
 
 void setup()
 {
@@ -50,64 +76,72 @@ void setup()
   Serial.begin(115200);
   delay(10);
 
-  Serial.println("Sensor Test");
+  Serial.println();
+  Serial.print("Sensor test... ");
   if (!bme.begin(0x76))
   {
     Serial.println("BME280@0x76 not detected");
-    if (!bme.begin(0x77))
-    {
-      Serial.println("BME280@0x77 not detected");
-      delay(1);
-      while (1);
-    }
+    restart();
   }
   else
   {
-    Serial.println("BME280 ready.");
+    Serial.println("BME280@0x76 ready");
   }
 
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
+  Serial.print(WLAN_SSID);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(WLAN_SSID, WLAN_PASS);
+
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
 
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print(" connected. ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
   mqtt.subscribe(&boiler_onoff);
+
+  Serial.println("Entering main loop...");
+
+#ifdef NO_SERIAL_OUTPUT
+  Serial.println("Serial output disabled for main loop.");
+  delay(5000);
+  yield();
+  Serial.end();
+#endif
 }
 
 void loop()
 {
+  Serial.println();
 
   // Ensure the connection to the MQTT server is alive (this will make the first
   // connection and automatically reconnect when disconnected)
-  MQTT_connect();
-  
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(1000)))
+  bool mqtt_connected = MQTT_connect();
+
+  if (mqtt_connected)
   {
-    if (subscription == &boiler_onoff)
+    Adafruit_MQTT_Subscribe *subscription;
+    while ((subscription = mqtt.readSubscription(1000)))
     {
-      if (strcmp((char *)boiler_onoff.lastread, "ON") == 0)
+      if (subscription == &boiler_onoff)
       {
-        Serial.println("Turning boiler ON");
-        digitalWrite(OUTPUT_PIN, HIGH);
-      }
-      else
-      {
-        Serial.println("Turning boiler OFF");
-        digitalWrite(OUTPUT_PIN, LOW);
+        if (strcmp((char *)boiler_onoff.lastread, "1") == 0)
+        {
+          Serial.println("Turning boiler ON");
+          digitalWrite(OUTPUT_PIN, HIGH);
+        }
+        else
+        {
+          Serial.println("Turning boiler OFF");
+          digitalWrite(OUTPUT_PIN, LOW);
+        }
       }
     }
   }
@@ -115,78 +149,53 @@ void loop()
   float temperature = bme.readTemperature();
   Serial.print("Temperature: ");
   Serial.print(temperature);
-  Serial.println(" °C");
+  Serial.print(" °C    ");
+  if (mqtt_connected) publishTopic(&temperature_topic, temperature);
 
   float pressure = bme.readPressure() / 100.0F;
   Serial.print("Pressure:    ");
   Serial.print(pressure);
-  Serial.println(" hPa");
+  Serial.print(" hPa ");
+  if (mqtt_connected) publishTopic(&pressure_topic, pressure);
 
   float humidity = bme.readHumidity();
-  Serial.print("Humidity:    ");
+  Serial.print("Humidity:    ");
   Serial.print(humidity);
-  Serial.println(" %");
-  
-  Serial.print("Publish Temperature: ");
-  if (!temperature_topic.publish(temperature))
-  {
-    Serial.println("Failed");
-  }
-  else
-  {
-    Serial.println("OK");
-  }
-
-  Serial.print("Publish Pressure: ");
-  if (!pressure_topic.publish(pressure))
-  {
-    Serial.println("Failed");
-  }
-  else
-  {
-    Serial.println("OK");
-  }
-
-  Serial.print("Publish Humidity: ");
-  if (! humidity_topic.publish(humidity))
-  {
-    Serial.println("Failed");
-  }
-  else
-  {
-    Serial.println("OK!");
-  }
+  Serial.print(" %     ");
+  if (mqtt_connected) publishTopic(&humidity_topic, humidity);
 
   delay(MAIN_LOOP_DELAY);
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
 // Should be called in the loop function and it will take care if connecting.
-void MQTT_connect()
+bool MQTT_connect()
 {
   int8_t ret;
 
   // Stop if already connected.
   if (mqtt.connected())
   {
-    return;
+    return true;
   }
 
   Serial.print("Connecting to MQTT... ");
 
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0)
-  { // connect will return 0 for connected
+  uint8_t retries = MQTT_RETRIES;
+  while ((ret = mqtt.connect()) != 0)  // connect will return 0 for connected
+  {
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
-    delay(5000);  // wait 5 seconds
+    delay(5000);
     retries--;
     if (retries == 0)
     {
-      // basically die and wait for WDT to reset me
-      while (1);
+      Serial.println(FAIL_STR);
+      return false;
     }
   }
-  Serial.println("MQTT Connected!");
+
+  Serial.println(OK_STR);
+  return true;
 }
